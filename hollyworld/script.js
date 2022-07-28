94edcec4ba9917b969c6121e8b12c37800aaf6d9
Md areeb Uddin <54110961+khan632@users.noreply.github.com>
2022-07-28 19:32:37 +0530

    Update script.js

--- hollyworld/script.js
+++ hollyworld/script.js
@@ -1,39 +1,3 @@
-/* 
-🌟 APP: Make Netflix
-
-Here we have the Netflix app but it's up to you to make it work by pulling all the movies using an API!
-
-Create a fetchMovies() function that will make a dynamic API call to what you need 👇
-========================================
-
-- fetchMovies()
-
-** fetchMovies takes in an URL, a div id or class from the HTML, and a path (poster or backdrop)
-
-
-
-These are the 3 main functions and their URL'S you must create  👇
-========================================
-
-- getOriginals()
-  * URL : 'https://api.themoviedb.org/3/discover/tv?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213'
-
-- getTrendingNow()
-  * URL : 'https://api.themoviedb.org/3/trending/movie/week?api_key=19f84e11932abbc79e6d83f82d6d1045'
-
-- getTopRated()
-  * URL : 'https://api.themoviedb.org/3/movie/top_rated?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US&page=1'
-
-
-** These functions will provide the URL you need to fetch() movies of that genere **
-
-These are all the DIV ID's you're gonna need access to 👇
-========================================================
-#1 CLASS 👉 'original__movies' = Div that holds Netflix Originals
-#2 ID 👉 'trending' = Div that holds trending Movies
-#3 ID 👉 'top_rated' = Div that holds top rated Movies
-*/
-
 // Call the main functions the page is loaded
 window.onload = () => {
   getOriginals()
@@ -56,7 +20,6 @@ function fetchMovies(url, dom_element, path_type) {
   }).catch(error => {
     console.log(error)
   })
-  // Within Fetch get the response and call showMovies() with the data , dom_element, and path type
 }
 
 //  ** Function that displays the movies to the DOM **
@@ -151,7 +114,6 @@ const handleMovieSelection = e => {
 
   // open modal
   $('#trailerModal').modal('show')
-  // we need to call the api with the ID
 }
 
 
